afbaf6d58397ebd67c10d03109838cbb6436477a
Dushane Smith <dushanesmith@gmail.com>
2024-09-17 19:47:15 -0400

    commiting some set up changes to build.gradle and colors and themes

--- app/src/main/java/com/dushanesmith/fetchrewardscodingexercise/ui/theme/Color.kt
+++ app/src/main/java/com/dushanesmith/fetchrewardscodingexercise/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SkyBlue = Color(0xff2bd9fe)
+val EerieBlack = Color(0xFF252627)
+val UclaBlue = Color(0xFF2274A5)
+val WhiteSmoke = Color(0xFFF1EDEE)
